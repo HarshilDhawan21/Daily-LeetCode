0b15a991eefeb11f9e469e0accd64eae05a1a9c2
Harshil Dhawan <mr07harshil@gmail.com>
2026-07-22 14:54:13 +0530

    Add TwoSum, LongestSubstring, and MedianOfTwoSortedArrays solutions

---
+++ Longest Substring without Repeating characters.py
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        int maxl = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left));
+                left++; }
+            charSet.add(s.charAt(right));
+            maxl = Math.max(maxl, right - left +1);}
+        return maxl;
+    }
+}
